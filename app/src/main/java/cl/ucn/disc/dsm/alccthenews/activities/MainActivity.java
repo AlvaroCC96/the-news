a01d40dc9f790d47ef5320b919fc59cefb21aed9
Álvaro Lucas Castillo Calabacero <alvarolucascc96@gmail.com>
2020-03-07 20:37:54 -0300

    feat(MainActivity): Implementation for Main Activity file in its respective package

---
+++ app/src/main/java/cl/ucn/disc/dsm/alccthenews/activities/MainActivity.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright [2020] [Alvaro Lucas Castillo Calabacero]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cl.ucn.disc.dsm.alccthenews.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import cl.ucn.disc.dsm.alccthenews.databinding.ActivityMainBinding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Main Launcher Activity.
+ *
+ * @author Alvaro Lucas Castillo Calabacero
+ */
+public class MainActivity extends AppCompatActivity {
+
+  /**
+   * The Logger
+   */
+  private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
+
+  /**
+   * @param savedInstanceState to use.
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Inflate the layout
+    ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+    // Assign to the main view.
+    setContentView(binding.getRoot());
+
+    // Set the toolbar
+    {
+      this.setSupportActionBar(binding.toolbar);
+    }
+
+    // The refresh
+    {
+      binding.swlRefresh.setOnRefreshListener(() -> {
+        log.debug("Refreshing ..");
+      });
+    }
+
+  }
+
+}
