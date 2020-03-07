24ef9127ae71895b74d092547b1063bb801c301c
Álvaro Lucas Castillo Calabacero <alvarolucascc96@gmail.com>
2020-03-07 13:28:08 -0300

    build(main): creation of the main application file and configuration of the gradle file

---
+++ app/src/main/java/cl/ucn/disc/dsm/alccthenews/MainApplication.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright [2020] [Alvaro Lucas Castillo Calabacero]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cl.ucn.disc.dsm.alccthenews;
+
+import android.app.Application;
+import androidx.appcompat.app.AppCompatDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Alvaro Lucas Castillo Calabacero
+ */
+public class MainApplication extends Application {
+
+  /**
+   * The Logger.
+   */
+  private static final Logger log = LoggerFactory.getLogger(MainApplication.class);
+
+  /**
+   * Called when the application is starting, before any activity, service, or receiver objects (excluding content
+   * providers) have been created.
+   */
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Day and Night support
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+
+    log.debug("Initializing: Done.");
+  }
+}
