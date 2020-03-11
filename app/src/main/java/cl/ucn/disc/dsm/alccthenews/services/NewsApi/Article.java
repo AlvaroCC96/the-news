27ca0d12511d6bc1d271f27244acdb7d91e9aebb
Álvaro Lucas Castillo Calabacero <alvarolucascc96@gmail.com>
2020-03-10 22:47:48 -0300

    feat(NewsApi): implementation of the NewsApi service model

---
+++ app/src/main/java/cl/ucn/disc/dsm/alccthenews/services/NewsApi/Article.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright [2020] [Alvaro Lucas Castillo Calabacero]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cl.ucn.disc.dsm.alccthenews.services.NewsApi;
+
+/**
+ * @author Alvaro Lucas Castillo Calabacero
+ */
+public class Article {
+  public Source source;
+  public String author;
+  public String title;
+  public String description;
+  public String url;
+  public String urlToImage;
+  public String publishedAt;
+  public String content;
+}
